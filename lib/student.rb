6c8568feb44a9d1450014fcd27b9f2cecdcdb18a
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 02:53:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
     attr_accessor :hello, :raise_hand
 
     def hello
-          "Hey there! I'm so excited to learn stuff."
+         puts "Hey there! I'm so excited to learn stuff."
 
     end
 
