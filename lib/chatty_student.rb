2a899445ff8b1444b87fd50ac9f3a033db8d6e47
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 02:57:50 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,3 +1,21 @@
+
 class ChattyStudent < Student
+
+    attr_accessor :hello, :raise_hand
+
+    def hello
+
+        # `"#{super}.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"`
+        # `"#{super}"`
+        super
+        puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...".to_s
+
+    end
+
+    def raise_hand
+
+         10.times{ super }.to_s
+
+    end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,16 @@
+
 class Student
 
-    attr_accessor
+    attr_accessor :hello, :raise_hand
 
-    
+    def hello
+         puts "Hey there! I'm so excited to learn stuff."
+
+    end
+
+    def raise_hand
+        puts "Pick me!"
+    end
+
 
 end
