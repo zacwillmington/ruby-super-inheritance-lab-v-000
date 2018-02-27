d9e20758610da68c02d521d11658bd9206a71df5
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 02:56:39 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,7 +8,7 @@ class ChattyStudent < Student
         # `"#{super}.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"`
         # `"#{super}"`
         super
-        "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+        "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n".to_s
 
     end
 
