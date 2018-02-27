ed6d7830bf28f524a84e3a79cbdf9ac5dcacefcc
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 02:52:08 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,3 +1,23 @@
+require 'student.rb'
+
 class ChattyStudent < Student
+
+    attr_accessor :hello, :raise_hand
+
+    def hello
+
+        # `"#{super}.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"`
+        # `"#{super}"`
+
+        "Hey there! I'm so excited to learn stuff.
+        How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+
+    end
+
+    def raise_hand
+
+         10.times{ super }.to_s
+
+    end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,17 @@
+
+#require 'pry'
+
 class Student
 
-    attr_accessor
+    attr_accessor :hello, :raise_hand
+    def hello
+          puts "Hey there! I'm so excited to learn stuff."
+        #binding.pry
+    end
 
-    
+    def raise_hand
+        puts "Pick me!"
+    end
+
 
 end
